27ca377f06c94d145d31ed0bfe931e6dc10b6633
SPR1NG9ER <69731339+SPR1NG9ER@users.noreply.github.com>
2020-11-01 19:47:39 +0300

    React-app API GOT

---
+++ src/components/errorMessage/errorMessage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './errorMessage.css';
+
+const ErrorMessage = () => {
+  return (
+    <>
+      <img src={'https://media4.giphy.com/media/26uf6qaxqHpYXgjWU/200.gif'} alt='error'></img>
+    </>
+  )
+}
+
+export default ErrorMessage;
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-spinner-p8v81yr4vv"><div className="ldio-qv8g6u77o5">
+      <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+    </div></div>
+  )
+}
+
+export default Spinner;
